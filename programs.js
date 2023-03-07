92322b158dc3a7008b5eaaf5b4a9354546661af5
Naman <naman.alphabin@gmail.com>
2023-03-07 19:03:20 +0530

    old programs

---
+++ programs.js
@@ -0,0 +1,68 @@
+// 1) alphabetical order
+
+// function alphaOrder(str)
+// {
+//     var arr = str.split("")
+//     arr.sort()
+//     var sortedStr = arr.join("")
+//     return sortedStr
+// }
+// console.log(alphaOrder('webmaster'));
+
+// 2) Pan card number
+
+// let length = 5
+
+// const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let result = "";
+// const charactersLength = characters.length;
+// for (let i = 0; i < length; i++) {
+//   result += characters.charAt(Math.floor(Math.random() * charactersLength));
+// }
+
+// let digit = Math.floor(Math.random() * (1111 - 4999 + 1)) + 4999;
+
+// console.log(result + digit);
+
+
+// 3) unique characters
+
+// function unique_char(str1)
+// {
+//  var str=str1;
+//  var uniql="";
+//  for (var x=0;x < str.length;x++)
+//  {
+
+//  if(uniql.indexOf(str.charAt(x))==-1)
+//   {
+//   uniql += str[x];  
+  
+//    }
+//   }
+//   return uniql;  
+// }  
+// console.log(unique_char("thequickbrownfoxjumpsoverthelazydog"));
+
+// 4) FizzBuzz challenge
+
+// for(i=1; i<101; i++)
+// {
+//     if(i%3==0)
+//     {
+//         console.log("Fizz");
+//     }
+//     else if(i%5==0)
+//     {
+//         console.log("Buzz");
+//     }
+//     else
+//     {
+//         console.log(i);
+//     }
+    
+// }
+
+
+// 5) Promise timeout
+
